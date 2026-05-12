ae366488ef5ba7cacd6bbe323dc150bf96936d6f
Ramariarison <andryramariarison@gmail.com>
2026-05-12 22:27:05 +0300

    feat: + méthodes login et logout dans auth controleur, guard par defaut modifié

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
 use App\Models\User as ModelsUser;
-use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -27,13 +28,61 @@ class AuthController extends Controller
             'avatar' => $avatarPath,
         ]);
 
-        // Génération du token JWT
-        $token = JWTAuth::fromUser($user);
-
         return response()->json([
             'message' => 'Utilisateur créé avec succès',
-            'user' => $user,
-            'token' => $token,
+            'user' => $user
         ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        // Validation de données
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string|min:6'
+        ]);
+
+        $credentials = $request->only(['email', 'password']);
+
+        // Attribution du token si credentials valident
+        if(! $token = Auth::attempt($credentials)){
+            return response()->json([
+                'message' => 'Please verify your credentials !'
+            ]);
+        };
+
+        // Récupération de l'utilisateur
+        $user = Auth::user();
+
+        // Vérification statut
+        if(! $user->status){
+            // Déconnexion
+            Auth::logout();
+
+            return response()->json([
+                'message' => 'Your account is not valided yet, please wait until the administrator valid it.',
+                'user_status' => $user->status,
+                'need_validation' => true
+            ], 403);
+        };
+
+        // Réponse json pour la vue
+        return response()->json([
+            'message' => 'Connected successfully !',
+            'user' => $user,
+            'access_token' => $token,
+            'type_token' => 'bearer',
+            'expires_in' => Auth::factory()->getTTL() * 60
+        ]);
+    }
+
+    // Déconnexion utilisateur
+    public function logout()
+    {
+        Auth::logout();
+
+        return response()->json([
+            'message' => 'Deconnected successfully !'
+        ]);
     }
 }
